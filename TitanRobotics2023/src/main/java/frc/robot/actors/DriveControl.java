c94cfccf02d331066213a783338095d8558f8ba2
aadams235 <122327119+aadams235@users.noreply.github.com>
2023-03-03 19:01:22 -0700

    Update DriveControl.java

--- TitanRobotics2023/src/main/java/frc/robot/actors/DriveControl.java
+++ TitanRobotics2023/src/main/java/frc/robot/actors/DriveControl.java
@@ -20,14 +20,7 @@ public class DriveControl
         motorFrontRight = new PWMVictorSPX(PortMap.FRONTRIGHT.portNumber);
         motorRearRight = new PWMVictorSPX(PortMap.REARRIGHT.portNumber);
     }
-
-    public void flightTankDrive(double flightY, double flightZ, double flightSLider) //Moves the sets of wheels based on respective inputs //comment out the motors here if using xboxTankDrive
-    {
-        //motorFrontLeft.set((-flightY + flightZ) * flightSLider);  //subtract 0.02 here from leftY for YaLikeJazz, slider is speed
-        //.set((-flightY + flightZ) * flightSLider); //add 0.02 here to leftY for YaLikeJazz
-        //motorFrontRight.set((flightY + flightZ + 0.015) * flightSLider); //add 0.015 here for And-You
-        //motorRearRight.set((flightY + flightZ + 0.015) * flightSLider); //add 0.015 here for And-You
-    }    
+    
     public void xboxTankDrive(double xboxLeftY, double xboxRightY, double xboxLeftX, double xboxRightX) //Moves the sets of wheels based on respective inputs //comment out the motors here if using flightTankDrive
     {
         motorFrontLeft.set(-xboxLeftY + (0.5 * xboxRightX));  //subtract 0.02 here from leftY for YaLikeJazz
